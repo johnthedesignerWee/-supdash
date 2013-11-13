c6a254a2030c362751e45fe9d542c4e2457aef6a
hrazith <hrazith@gmail.com>
2013-11-12 21:43:30 -0500

    alert pop-ups and sort arrow

--- _includes/comms.php
+++ _includes/comms.php
@@ -4,7 +4,7 @@
         <p class="message">3Q 2013 List of PMP accounts tracking toward termination and fee forfeiture on October 15</p>
         <div class="close"><span class="icon-close"></span></div>
     </div>
-    <a class="call-modal" href="#alert-message">
+    
     <div class="communication">
         <span class="label">Nov 01</span><span class="severity">Attention Required</span>
         <p class="message">3Q 2013 List of PMP accounts tracking toward termination and fee forfeiture on October 15</p>
@@ -19,25 +19,31 @@
         <div class="close"><span class="icon-close"></span></div>
     </div>
     
+    <a class="call-modal" href="#alert-message">
     <div class="communication">
         <span class="label">Nov 01</span>
         <span class="severity">Alert</span>
         <p class="message">3Q 2013 List of PMP accounts tracking toward termination and fee forfeiture on October 15</p>
         <div class="close"><span class="icon-close"></span></div>
     </div>
+    </a>
     
+    <a class="call-modal" href="#alert-message">
     <div class="communication">
         <span class="label">Nov 01</span>
         <span class="severity">Alert</span>
         <p class="message">3Q 2013 List of PMP accounts tracking toward termination and fee forfeiture on October 15</p>
         <div class="close"><span class="icon-close"></span></div>
     </div>
+    </a>
+    <a class="call-modal" href="#alert-message">
     <div class="communication">
         <span class="label">Nov 01</span>
         <span class="severity">Alert</span>
         <p class="message">3Q 2013 List of PMP accounts tracking toward termination and fee forfeiture on October 15</p>
         <div class="close"><span class="icon-close"></span></div>
     </div>
+    </a>
     <div class="communication">
         <span class="label">Nov 01</span>
         <span class="severity">Alert</span>
